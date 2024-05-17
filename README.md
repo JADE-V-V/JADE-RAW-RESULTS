b9787f0659aada88593b77d109cbad09ffd37d40
Davide Laghi <davide.laghi01@gmail.com>
2024-05-17 13:21:15 +0200

    added Tiara-BS

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
 [![Metadata-check](https://github.com/JADE-V-V/JADE-RAW-RESULTS/actions/workflows/metadata_check.yml/badge.svg?branch=main)](https://github.com/JADE-V-V/JADE-RAW-RESULTS/actions/workflows/metadata_check.yml)
 
 # JADE-RAW-RESULTS
-This repo hosts the raw data coming from the post-processing of JADE assessments
+This repo hosts the raw data coming from the post-processing of JADE assessments.
+
+## Uploading new results
+For the vast majority of cases, adding data to this repository is pretty straightforward, the .csv files that are output by JADE in the
+`Raw_Data` folders, can be directly copied in the corresponding folder in this repository structure, which looks like the following:
+```
+ROOT
+  |-----<lib1>
+  |       |-----<>
+  |
+  |-----<lib2>
+          |-----<benchmark_name>
+                      |-----------<mcnp>
+                      |             |------<Raw_Data>
+                      |                        |--------<raw_data1>.csv
+                      |                        |--------<raw_data2>.csv
+                      |                        |--------<...>
+                      |                        |--------metadata.json
+                      |
+                      |-----------<openmc>
+                      |-----------<...>
+```
+
+Some sanity checks are performed on the `metadata.json` keywords and values to ensure they are consistent with the allowed library names and with the folder structure they are inserted into (for more details check the [CI tests](./tests/metadata_test.py)).
+
+### Exceptions
+#### Tiara-BS
+The .csv raw data structure in this benchmark is not compatible with JADE results. To correclty import them into this repository use this [script](./utils/import_tiara_bs.py). 
---
+++ utils/import_tiara_bs.py
@@ -0,0 +1,42 @@
+"""Import the Tiara-BS benchmark from JADE results to the WebbApp format.
+
+The Tiara-BS benchmark in JADE has a .csv raw data structure that is not 
+directly compatible with the WebbApp. The following scritp needs to be run to
+import JADE's raw data into the WebbApp format. Do not forget to manually add
+or copy the metadata.json file associated to the raw data that is being imported.
+
+SOURCE: path to the JADE raw data .csv files.
+DEST: path to the destination folder in the JADE RAW RESULTS repository.
+"""
+
+import os
+import pandas as pd
+
+# In Tiara BS, of each csv, only the total is needed. Hence, we need to re-compact them into a single csv for the plot
+SOURCE = r"R:\AC_ResultsDB\Jade\04_JADE_latest_root\Tests\Post-Processing\Comparisons\Exp_Vs_33c_Vs_32c\Tiara-BS\mcnp\Raw_Data\32c"
+DEST = (
+    r"D:\DATA\laghida\Documents\GitHub\JADE-RAW-RESULTS\ROOT\32c\Tiara-BS\mcnp\Raw_Data"
+)
+
+# dictionary for .csv
+tallies = {"14": "Bare", "24": "15 mm", "34": "30 mm", "44": "50 mm", "54": "90 mm"}
+thicknesses = {
+    "cc": {"43": ["25-40", "50-40", "100-00", "150-00"], "68": ["50-00", "100-00"]},
+    "fe": {"43": ["20-00", "40-00", "100-00"], "68": ["20-00", "40-00", "100-00"]},
+}
+
+
+for material in ["cc", "fe"]:
+    for energy in ["43", "68"]:
+        for thickness in thicknesses[material][energy]:
+            rows = []
+            for tally in ["14", "24", "34", "44", "54"]:
+                file = f"{material}-{energy}-{thickness} {tally}.csv"
+                df = pd.read_csv(os.path.join(SOURCE, file))
+                df["Cells"] = tallies[tally]
+                df = df.iloc[-1]
+                rows.append(df)
+            df = pd.concat(rows, axis=1).T
+            simp_thick = thickness.split("-")[0]
+            outfile = os.path.join(DEST, f"{material}-{energy}-{simp_thick}.csv")
+            df.to_csv(outfile, index=False)
