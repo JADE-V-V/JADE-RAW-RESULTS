e3b442cf8e5eb3f41c500535931ddb8a6b0919e7
mcampos16 <Marta.Campos@ext.f4e.europa.eu>
2025-10-08 15:29:47 +0200

    Update of raw results to match JADE v4 + update of import scripts

--- utils/import_all.py
+++ utils/import_all.py
@@ -11,13 +11,9 @@ import json
 import os
 import shutil
 
-from import_fng_sic import import_sic
-from import_tiara_bs import import_bs
-from import_tiara_fc import import_fc
-
 # --- Mandatory user parameters ---
 # Source folder where the results are stored
-SRC = r"R:\AC_ResultsDB\Jade\04_JADE_latest_root\Tests\Post-Processing"
+SRC = r"R:\AC_ResultsDB\Jade\06_JADEv4\raw_data"
 # Destination folder where the results will be copied
 ROOT_DEST = "ROOT"
 # if true the files are not copied but a preview of the missing results is printed
@@ -25,52 +21,34 @@ ONLY_SCREEN = False
 
 # --- Optional user parameters ---
 # exclude some libraries from the import. This libraries will be ignored
-EXCLUDE_LIBRARIES = ["40c", "92c", "99c"]  # e.g. ["33c", "99c", "40c"]
+EXCLUDE_LIBRARIES = []  # e.g. ["_mcnp_-_FENDL 3.2c_", "_openmc_-_JEFF-3.3_"]
 # This is used if an automatic metadata file needs to be added.
-LIB_NAMES = {
-    "21c": "FENDL 2.1c",
-    "30c": "FENDL 3.0",
-    "31c": "FENDL 3.1d",
-    "32c": "FENDL 3.2b",
-    "32d": "FENDL 3.2c",
-    "70c": "ENDFB VII.0",
-    "00c": "ENDFB VIII.0",
-    "34y": "IRDFF II",
-    "03c": "JEFF 3.3",
-    "99c": "D1SUNED (FENDL 3.1d+EAF2007)",
-    "93c": "D1SUNED (FENDL 3.2b+TENDL2017)",
-    "exp": "experiment",
-}
+
 # This is used if an automatic metadata file needs to be added.
 BASE_METADATA = {
-    "jade_version": "3.0.1",
-    "code": "mcnp",  # This will be replaced by the code name found in folder
-    "code_version": "6.2",
-    "library": "ENDF",  # This will be replaced by the lib name found in folder
-    "benchmark_name": "JEFF 3.3",  # This will be replaced by the lib name found in folder
+    "jade_version": "4.3.0",
+    "code_version": None,
     "benchmark_version": "1.0",
-    "jade_run_version": "3.0.1",
+    "jade_run_version": "4.3.0",
 }
 
 
-def result_available(lib: str, benchmark: str, code: str) -> bool:
+def result_available(code_lib: str, benchmark: str) -> bool:
     """Check if the results are already present in ROOT
 
     Parameters
     ----------
-    lib : str
-        library suffix
+    code_lib : str
+        "_code_-_library_" combination
     benchmark : str
         benchmark name
-    code : str
-        code name
 
     Returns
     -------
     bool
         True if the raw data folder is already present, False otherwise
     """
-    path = os.path.join(ROOT_DEST, lib, benchmark, code, "Raw_Data")
+    path = os.path.join(ROOT_DEST, code_lib, benchmark)
     if os.path.exists(path):
         # If it is empty remove it
         if len(os.listdir(path)) == 0:
@@ -83,141 +61,78 @@ def result_available(lib: str, benchmark: str, code: str) -> bool:
         return False
 
 
-def sorted_listdir(directory) -> list[str]:
-    """List the directory content sorted by creation time
+def copy_results(src: str, code_lib: str, benchmark: str, metadata: dict = None):
+    """Copy the results from the source to the destination.
 
     Parameters
     ----------
-    directory : os.PathLike
-        Path to the directory
-
-    Returns
-    -------
-    list[str]
-        List of items in the directory sorted by creation time
-    """
-
-    def get_creation_time(item):
-        item_path = os.path.join(directory, item)
-        return os.path.getctime(item_path)
-
-    items = os.listdir(directory)
-    sorted_items = sorted(items, key=get_creation_time, reverse=True)
-    return sorted_items
-
-
-def copy_results(
-    src: os.PathLike, lib: str, benchmark: str, code: str, metadata: dict = None
-):
-    """Copy the results from the source to the destination. This accounts for
-    specific benchmarks needs like in Tiara case.
-
-    Parameters
-    ----------
-    src : os.PathLike
+    src : str
         folder to be copied
-    lib : str
-        library suffix
+    code_lib : str
+        "_code_-_library_" combination
     benchmark : str
         benchmark name
-    code : str
-        code name
     metadata : dict, optional
-        if not None, a metadata file will be added to the newly copied files, by default None
+        if None, a metadata file will be added to the newly copied files, by default None
+        if not None, the metadata file present in the source folder will be copied
     """
-    dest = os.path.join(ROOT_DEST, lib, benchmark, code, "Raw_Data")
-    if benchmark == "Tiara-BS":
-        if not os.path.exists(dest):
-            os.makedirs(dest)
-        import_bs(src, dest)
-    elif benchmark == "Tiara-FC":
-        if not os.path.exists(dest):
-            os.makedirs(dest)
-        import_fc(src, dest)
-    elif benchmark == "FNG-SiC":
-        if not os.path.exists(dest):
-            os.makedirs(dest)
-        import_sic(src, dest)
-    else:
-        shutil.copytree(src, dest)
-    if metadata is not None:
+    dest = os.path.join(ROOT_DEST, code_lib, benchmark)
+    shutil.copytree(src, dest)
+    if metadata is None and code_lib != "_exp_-_exp_":
         # Perform your action here
-        new_dict = metadata.copy()
-        new_dict["code"] = code
+        new_dict = {}
+        new_dict["benchmark_version"] = BASE_METADATA["benchmark_version"]
+        new_dict["jade_run_version"] = BASE_METADATA["jade_run_version"]
         new_dict["benchmark_name"] = benchmark
-        new_dict["library"] = LIB_NAMES[lib]
+        if code_lib.split("-")[1] == "_ENDFB" or code_lib.split("-")[1] == "_JEFF":
+            new_dict["library"] = (
+                code_lib.split("-")[1].replace("_", "")
+                + "-"
+                + code_lib.split("-")[2].replace("_", "")
+            )
+        else:
+            new_dict["library"] = code_lib.split("-")[1].replace("_", "")
+        new_dict["code"] = code_lib.split("-")[0].replace("_", "")
+        new_dict["jade_version"] = BASE_METADATA["jade_version"]
+        new_dict["code_version"] = BASE_METADATA["code_version"]
         with open(os.path.join(dest, "metadata.json"), "w", encoding="utf-8") as f:
-            json.dump(new_dict, f)
+            json.dump(new_dict, f, indent=4)
 
 
 def main() -> None:
     """Main function to import all the results from the source to the destination"""
     print("Importing computational results...")
     # First deal with computational benchmarks
-    comp_path = os.path.join(SRC, "Single_Libraries")
-    for lib in os.listdir(comp_path):
+    for code_lib in os.listdir(SRC):
         # skip libraries that should be ignored
-        if lib in EXCLUDE_LIBRARIES:
+        if code_lib in EXCLUDE_LIBRARIES:
             continue
-        lib_path = os.path.join(comp_path, lib)
-        for benchmark in os.listdir(lib_path):
-            bench_path = os.path.join(lib_path, benchmark)
-            for code in os.listdir(bench_path):
-                raw_data_folder = os.path.join(bench_path, code, "Raw_Data")
-                available = result_available(lib, benchmark, code)
-                if not available:
-                    # Print the missing results
-                    if ONLY_SCREEN:
-                        print(f"{lib} {benchmark} {code}")
-                    # copy the missing results
-                    else:
-                        print(f"{lib} {benchmark} {code}")
-                        copy_results(
-                            raw_data_folder,
-                            lib,
-                            benchmark,
-                            code,
-                            metadata=BASE_METADATA,
-                        )
-
-    # Then deal with experimental benchmarks
-
-    # get the list of folders by creation date. In this way the folder will be
-    # copied the first time it is found (newer) and if repeated in older assessments
-    # it will be ignored as it is already present
-    print("Importing experimental results...")
-    already_printed = []
-    exp_path = os.path.join(SRC, "Comparisons")
-    for comparison_folder in sorted_listdir(exp_path):
-        # Only interesterd in experimental benchmarks here
-        if comparison_folder[:3] != "Exp":
-            continue
-        comparison_path = os.path.join(exp_path, comparison_folder)
-        for benchmark in os.listdir(comparison_path):
-            bench_path = os.path.join(comparison_path, benchmark)
-            for code in os.listdir(bench_path):
-                raw_data_folder = os.path.join(bench_path, code, "Raw_Data")
-                for lib in os.listdir(raw_data_folder):
-                    if lib in EXCLUDE_LIBRARIES:
-                        continue
-                    available = result_available(lib, benchmark, code)
-                    if not available:
-                        # Print the missing results
-                        if ONLY_SCREEN:
-                            stringa = f"{lib} {benchmark} {code}"
-                            if stringa not in already_printed:
-                                already_printed.append(stringa)
-                                print(stringa)
-                        # copy the missing results
-                        else:
-                            print(f"{lib} {benchmark} {code}")
-                            copy_results(
-                                os.path.join(raw_data_folder, lib),
-                                lib,
-                                benchmark,
-                                code,
-                                metadata=BASE_METADATA,
-                            )
+        code_lib_path = os.path.join(SRC, code_lib)
+        for benchmark in os.listdir(code_lib_path):
+            bench_path = os.path.join(code_lib_path, benchmark)
+            available = result_available(code_lib, benchmark)
+            try:
+                with open(
+                    os.path.join(bench_path, "metadata.json"), encoding="utf-8"
+                ) as f:
+                    metadata = json.load(f)
+            except FileNotFoundError:
+                metadata = None
+            if not available:
+                # Print the missing results
+                if ONLY_SCREEN:
+                    print(
+                        f"Missing results for {code_lib} {benchmark}. Since ONLY_SCREEN is set to True, no files will be imported."
+                    )
+                # copy the missing results
+                else:
+                    print(f"{code_lib} {benchmark}")
+                    copy_results(
+                        bench_path,
+                        code_lib,
+                        benchmark,
+                        metadata,
+                    )
 
 
 if __name__ == "__main__":
